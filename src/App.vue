00724898ba3f0204665c19b700ec46995b74e61a
dymafr <erwan1309@hotmail.com>
2022-03-15 18:23:49 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <form>
     <div>
-      <input
-        :class="{
-          success:
-            meta.touched && meta.valid && meta.validated && !meta.pending,
-          error: meta.touched && !meta.valid && meta.validated && !meta.pending,
-        }"
-        @focus="handleBlur"
-        @blur="handleChange"
-        v-model="usernameValue"
-        type="text"
-        placeholder="Prénom"
-      />
+      <input v-model="usernameValue" type="text" placeholder="Prénom" />
     </div>
-    <p v-if="meta.pending">Chargement...</p>
-    <p v-if="errorMessage">{{ errorMessage }}</p>
-    <pre>{{ meta }}</pre>
-    <pre>{{ metaForm }}</pre>
+    <button @click="triggerErreur()">erreur</button>
+    <button @click="triggerValue()">valeur</button>
   </form>
 </template>
 
@@ -26,43 +13,28 @@ import { useField, useForm } from 'vee-validate';
 import { z } from 'zod';
 import { toFieldValidator } from '@vee-validate/zod';
 
-const promise = (valeur: boolean) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(valeur);
-    }, 3000);
-  });
-
-const { meta: metaForm } = useForm();
+const {
+  errors: formErrors,
+  setFieldValue,
+  setValues,
+  setFieldError,
+  setErrors: setFormErrors,
+} = useForm();
 
 const {
   value: usernameValue,
   errorMessage,
-  meta,
-  handleBlur,
-  handleChange,
+  errors,
+  setValue,
+  setErrors,
 } = useField(
-  'password',
-  toFieldValidator(
-    z
-      .string()
-      .min(5, { message: 'Trop court !' })
-      .refine(
-        async (data) =>
-          (await data) === 'valid' ? promise(true) : promise(false),
-        { message: 'erreur' }
-      )
-  ),
-  { validateOnValueUpdate: false }
+  'username',
+  toFieldValidator(z.string().min(5, { message: 'Trop court !' }))
 );
+
+function tirggerErreur {
+  setFieldError('username', 'oops')
+}
 </script>
 
-<style scoped lang="scss">
-.success {
-  border-color: green;
-}
-
-.error {
-  border-color: red;
-}
-</style>
+<style scoped lang="scss"></style>
